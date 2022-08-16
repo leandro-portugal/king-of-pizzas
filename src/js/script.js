54b94dac1a9ba20f38149fd58cf933d1d61e1098
Leandro Portugal <leandroportugal@outlook.com.br>
2022-08-16 01:52:52 -0300

    Show modal to add items to cart

--- src/js/script.js
+++ src/js/script.js
@@ -9,5 +9,15 @@ pizza.querySelector('.pizza-item--name').innerHTML = item.name;
 pizza.querySelector('.pizza-item--desc').innerHTML = item.description;
 pizza.querySelector('.pizza-item--price').innerHTML = `A partir de R$ ${item.price[0].toFixed(2)}`;
 
+pizza.querySelector('a').addEventListener('click',(e)=>{
+  e.preventDefault();
+  qs('.pizzaWindowArea').style.opacity = 0;
+  qs('.pizzaWindowArea').style.display = 'flex';
+  setTimeout(()=>{
+    qs('.pizzaWindowArea').style.opacity = 1;
+  }, 200);
+
+});
+
 qs('.pizza-area').append(pizza);
 });
