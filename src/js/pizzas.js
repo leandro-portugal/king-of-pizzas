4e10d4b9f94d1eecd37e62b6872033f80744ca6d
Leandro Portugal <leandroportugal@outlook.com.br>
2022-08-17 14:21:48 -0300

    Listing the product information in the modal

--- src/js/pizzas.js
+++ src/js/pizzas.js
@@ -1,11 +1,11 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'src/images/pizza.png', price:[20.19, 40.19, 80.19 ], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:2, name:'Calabresa', img:'src/images/pizza2.png', price:[18.00, 36,00, 72.00 ], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:3, name:'Portuguesa', img:'src/images/pizza3.png', price:[17.45, 35.50, 65.90], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:4, name:'Lombo', img:'src/images/pizza4.png', price:[19.77, 40.19, 72.00], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:5, name:'Brócolis', img:'src/images/pizza5.png', price:[21.43, 36,00, 55.75], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:6, name:'Moda da Casa', img:'src/images/pizza6.png', price:[19.77, 40.19, 52.00], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:7, name:'Atum', img:'src/images/pizza7.png', price:[27.45, 45.50, 95.90], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
-    {id:8, name:'Vegana', img:'src/images/pizza8.png', price:[23.45, 35.50, 65.90], sizes:['4 Fatias', '8 Fatias', '16 Fatias'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'}
+    {id:1, name:'Mussarela', img:'src/images/pizza.png', price:[20.19, 40.19, 80.19 ], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:2, name:'Calabresa', img:'src/images/pizza2.png', price:[18.00, 36.00, 72.00 ], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:3, name:'Portuguesa', img:'src/images/pizza3.png', price:[17.45, 35.50, 65.90], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:4, name:'Lombo', img:'src/images/pizza4.png', price:[19.77, 40.19, 72.00], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:5, name:'Brócolis', img:'src/images/pizza5.png', price:[21.43, 36.00, 55.75], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:6, name:'Moda da Casa', img:'src/images/pizza6.png', price:[19.77, 40.19, 52.00], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:7, name:'Atum', img:'src/images/pizza7.png', price:[27.45, 45.50, 95.90], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'},
+    {id:8, name:'Vegana', img:'src/images/pizza8.png', price:[23.45, 35.50, 65.90], sizes:['350g', '700g', '1.5kg'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit.'}
 
 ];
--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,50 @@
 const qs = (elmt) => document.querySelector(elmt);
 const qsa = (elmt) => document.querySelectorAll(elmt);
 
-pizzaJson.map((item, index)=>{
-let pizza = qs('.models .pizza-item').cloneNode(true);
+let pizzaQuantity = 1;
 
-pizza.querySelector('.pizza-item--img img').src = item.img;
-pizza.querySelector('.pizza-item--name').innerHTML = item.name;
-pizza.querySelector('.pizza-item--desc').innerHTML = item.description;
-pizza.querySelector('.pizza-item--price').innerHTML = `A partir de R$ ${item.price[0].toFixed(2)}`;
+pizzaJson.map((item, index) => {
+  let pizza = qs('.models .pizza-item').cloneNode(true);
 
-pizza.querySelector('a').addEventListener('click',(e)=>{
-  e.preventDefault();
-  qs('.pizzaWindowArea').style.opacity = 0;
-  qs('.pizzaWindowArea').style.display = 'flex';
-  setTimeout(()=>{
-    qs('.pizzaWindowArea').style.opacity = 1;
-  }, 200);
+  pizza.setAttribute('data-key', index);
+  pizza.querySelector('.pizza-item--img img').src = item.img;
+  pizza.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizza.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizza.querySelector('.pizza-item--price').innerHTML = `A partir de R$ ${item.price[0].toFixed(2)}`;
 
-});
+  pizza.querySelector('a').addEventListener('click', (e) => {
 
-qs('.pizza-area').append(pizza);
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    pizzaQuantity = 1
+
+    e.preventDefault();
+
+    qs('.pizzaBig img').src = pizzaJson[key].img;
+    qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    qs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price[2].toFixed(2)}`;
+    qs('.pizzaInfo--size.selected').classList.remove('selected');
+
+    qsa('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if(sizeIndex == 2){
+        size.classList.add('selected');
+        
+      }
+
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]; 
+      
+    });;
+ 
+   
+    qs('.pizzaInfo--qt').innerHTML = pizzaQuantity;
+    qs('.pizzaWindowArea').style.opacity = 0;
+    qs('.pizzaWindowArea').style.display = 'flex';
+
+    setTimeout(() => {
+      qs('.pizzaWindowArea').style.opacity = 1;
+    }, 200);
+
+  });
+
+  qs('.pizza-area').append(pizza);
 });
